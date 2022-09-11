d788114d39ce6bc5351c990ebb6ecc47a7ff40e7
Ajelmar Medina <ajelmarmedina@gmail.com>
2022-09-12 00:30:06 +0800

    mobile scaling animation

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 window.onload = () => {
   window.scrollTo(0, 0);
+  
+  if (window.matchMedia(`(prefers-reduced-motion: reduce)`)) {observeInterests()}
+
   setTimeout(typeSubtitleEffect, 2000);
   setTimeout(() => {
     document.querySelector("body").style.overflowY = "scroll";
@@ -34,6 +37,28 @@ async function typeSubtitle(text, textSpan) {
 }
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function observeInterests() {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(async (entry) => {
+      const square = entry.target.querySelector('.interests');
+      
+      if (entry.isIntersecting) {
+        await sleep(1000);
+        square.classList.add('interests_animation');
+      return; // if we added the class, exit the function
+      }
+  
+      // We're not intersecting, so remove the class!
+      square.classList.remove('interests_animation');
+    });
+  });
+  
+  observer.observe(document.querySelector('.code-interest-wrapper'));
+  observer.observe(document.querySelector('.music-interest-wrapper'));
+  observer.observe(document.querySelector('.growth-interest-wrapper'));
+  observer.observe(document.querySelector('.support-interest-wrapper'));
 }
 
 window.onbeforeunload = function () {
