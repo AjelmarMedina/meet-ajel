ba14f1699fc3ad32799a296f594cde892b76ba37
ajelmarmedina@gmail.com <ajelmarmedina@gmail.com>
2023-08-30 09:27:34 +0800

    .

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ window.onload = () => {
 async function typeSubtitleEffect() {
   let pointer = document.querySelector("#subtitle-type_pointer-main");
 
-  let subtitleMain = "Frontend Web Developer".split("");
+  let subtitleMain = "Testing".split("");
   let subtitleMainSpan = document.querySelector("#subtitle-main");
 
   let subtitleStats =
-    `Age: 17, Height: 5'7", Section: COM233`.split("");
+    `Age: 18, Height: 5'7", Section: COM233`.split("");
   let subtitleStatsSpan = document.querySelector("#subtitle-stats");
 
   subtitleMainSpan.innerHTML = "";
